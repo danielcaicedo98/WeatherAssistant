daca044e1b39bce91aefb361d6672cfb6bfafb02
daniel98 <daniel.andres.caicedo@correounivalle.edu.co>
2025-04-15 18:09:45 -0500

    comandos por voz

--- src/routes.jsx
+++ src/routes.jsx
@@ -7,6 +7,7 @@ import {
 import { Home, Notifications, WeatherAssistant } from "@/pages/dashboard";
 import { SignIn, SignUp } from "@/pages/auth";
 import UserManual from "./pages/dashboard/user_manual";
+import SmartAssistant from "./pages/dashboard/smart_assistant";
 
 const icon = {
   className: "w-5 h-5 text-inherit",
@@ -28,20 +29,21 @@ export const routes = [
         path: "/assistant",
         element: <WeatherAssistant />,
       },
+      {
+        icon: <InformationCircleIcon {...icon} />,
+        name: "Asistente por voz",
+        path: "/smart_assistant",
+        element: <SmartAssistant />,
+      },
+      {
+        icon: <InformationCircleIcon {...icon} />,
+        name: "Manual de usuario",
+        path: "/usermanual",
+        element: <UserManual />,
+      }
 
     ],
 
-  },
-  {
-    layout: "dashboard",
-    pages: [
-      {
-        // icon: <InformationCircleIcon {...icon} />,
-        name: "",
-        path: "/user_manual",
-        element: <UserManual />,
-      }
-    ]
   },
 
   {
