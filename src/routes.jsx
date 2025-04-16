a2c3825964c1744f0a54a6d0c0d1041eb417d9c8
daniel98 <daniel.andres.caicedo@correounivalle.edu.co>
2025-04-15 20:45:36 -0500

    comandos por voz

--- src/routes.jsx
+++ src/routes.jsx
@@ -8,6 +8,7 @@ import { Home, Notifications, WeatherAssistant } from "@/pages/dashboard";
 import { SignIn, SignUp } from "@/pages/auth";
 import UserManual from "./pages/dashboard/user_manual";
 import SmartAssistant from "./pages/dashboard/smart_assistant";
+import { BookOpen } from "lucide-react";
 
 const icon = {
   className: "w-5 h-5 text-inherit",
@@ -29,14 +30,14 @@ export const routes = [
         path: "/assistant",
         element: <WeatherAssistant />,
       },
+      // {
+      //   icon: <InformationCircleIcon {...icon} />,
+      //   name: "Asistente por voz",
+      //   path: "/smart_assistant",
+      //   element: <SmartAssistant />,
+      // },
       {
-        icon: <InformationCircleIcon {...icon} />,
-        name: "Asistente por voz",
-        path: "/smart_assistant",
-        element: <SmartAssistant />,
-      },
-      {
-        icon: <InformationCircleIcon {...icon} />,
+        icon: <BookOpen {...icon} />,
         name: "Manual de usuario",
         path: "/usermanual",
         element: <UserManual />,
